5428f5615e88d9223bd5d5fe34767ad6d44cad6f
Jeremy Deutsch <jeremy.e.deutsch@gmail.com>
2020-08-12 20:50:02 -0700

    refactor collapsible section example to have more generic collapsible section component

--- example/src/CollapsibleSectionExample.tsx
+++ example/src/CollapsibleSectionExample.tsx
@@ -13,7 +13,13 @@ import goodPlaceBios from "./goodPlaceBios.json";
 // A collapsible page section with a sticky header. Uses three different
 // react-native-scrollable hooks to keep the page from jumping too much
 // when the header is stuck to the top and the user collapses the section.
-function CollapsibleSection(props: { title: string; body: string }) {
+function CollapsibleSection(props: {
+  renderStickyHeader: (args: {
+    isCollapsed: boolean;
+    toggleCollapsed: () => void;
+  }) => React.ReactElement | null;
+  children: React.ReactNode;
+}) {
   const [isCollapsed, setIsCollapsed] = React.useState(false);
 
   const getPositionInScrollView = useGetPositionInScrollView();
@@ -29,39 +35,48 @@ function CollapsibleSection(props: { title: string; body: string }) {
     });
   }, [addScrollListener]);
 
+  const toggleCollapsed = async () => {
+    if (!isCollapsed) {
+      if (!stickyHeaderViewRef.current) return;
+      // if we're not collapsed, but the sticky header is stuck to
+      // the top, we might have to jump the scroll view upwards so
+      // the sticky header is still at the top in the end.
+      try {
+        const { y } = await getPositionInScrollView(
+          stickyHeaderViewRef.current
+        );
+        if (scrollAmountRef.current > y) {
+          scrollViewRef.current?.getNode().scrollTo({ y, animated: false });
+        }
+      } catch (e) {
+        console.warn("Getting the sticky header position failed", e);
+      }
+    }
+    setIsCollapsed(!isCollapsed);
+  };
+
   return (
     <StickyHeaderView
       viewRef={stickyHeaderViewRef}
-      stickyHeaderElement={
-        <TouchableOpacity
-          onPress={async () => {
-            if (!stickyHeaderViewRef.current) return;
-            if (!isCollapsed) {
-              // if we're not collapsed, but the sticky header is stuck to
-              // the top, we might have to jump the scroll view upwards so
-              // the sticky header is still at the top in the end.
-              try {
-                const { y } = await getPositionInScrollView(
-                  stickyHeaderViewRef.current
-                );
-                if (scrollAmountRef.current > y) {
-                  scrollViewRef.current
-                    ?.getNode()
-                    .scrollTo({ y, animated: false });
-                }
-              } catch (e) {
-                console.warn("Getting the sticky header position failed", e);
-              }
-            }
-            setIsCollapsed(!isCollapsed);
-          }}
-        >
-          <Text style={styles.title}>{props.title}</Text>
-        </TouchableOpacity>
-      }
+      stickyHeaderElement={props.renderStickyHeader({
+        isCollapsed,
+        toggleCollapsed,
+      })}
     >
-      {!isCollapsed && <Text style={styles.body}>{props.body}</Text>}
+      {!isCollapsed && props.children}
     </StickyHeaderView>
+  );
+}
+
+function SectionTitle(props: {
+  children: React.ReactNode;
+  isCollapsed: boolean;
+  toggleCollapsed: () => void;
+}) {
+  return (
+    <TouchableOpacity onPress={props.toggleCollapsed}>
+      <Text style={styles.title}>{props.children}</Text>
+    </TouchableOpacity>
   );
 }
 
@@ -69,12 +84,41 @@ export default function CollapsibleSectionExample() {
   return (
     <EnhancedScrollView>
       <CollapsibleSection
-        title="Ellenor Shellstrop"
-        body={goodPlaceBios.ellenor}
-      />
-      <CollapsibleSection title="Chidi Anagonye" body={goodPlaceBios.chidi} />
-      <CollapsibleSection title="Jason Mendoza" body={goodPlaceBios.jason} />
-      <CollapsibleSection title="Tahani Al-Jamil" body={goodPlaceBios.tahani} />
+        renderStickyHeader={({ isCollapsed, toggleCollapsed }) => (
+          <SectionTitle {...{ isCollapsed, toggleCollapsed }}>
+            Ellenor Shellstrop
+          </SectionTitle>
+        )}
+      >
+        <Text style={styles.body}>{goodPlaceBios.ellenor}</Text>
+      </CollapsibleSection>
+      <CollapsibleSection
+        renderStickyHeader={({ isCollapsed, toggleCollapsed }) => (
+          <SectionTitle {...{ isCollapsed, toggleCollapsed }}>
+            Chidi Anagonye
+          </SectionTitle>
+        )}
+      >
+        <Text style={styles.body}>{goodPlaceBios.chidi}</Text>
+      </CollapsibleSection>
+      <CollapsibleSection
+        renderStickyHeader={({ isCollapsed, toggleCollapsed }) => (
+          <SectionTitle {...{ isCollapsed, toggleCollapsed }}>
+            Jason Mendoza
+          </SectionTitle>
+        )}
+      >
+        <Text style={styles.body}>{goodPlaceBios.jason}</Text>
+      </CollapsibleSection>
+      <CollapsibleSection
+        renderStickyHeader={({ isCollapsed, toggleCollapsed }) => (
+          <SectionTitle {...{ isCollapsed, toggleCollapsed }}>
+            Tahani Al-Jamil
+          </SectionTitle>
+        )}
+      >
+        <Text style={styles.body}>{goodPlaceBios.tahani}</Text>
+      </CollapsibleSection>
     </EnhancedScrollView>
   );
 }
